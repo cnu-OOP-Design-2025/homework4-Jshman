5e7abb4c83b7adaed8ae3c1d6e459b72c2b04f0b
JonghHyeon <jonghp1128@naver.com>
2025-09-30 14:10:06 +0900

    [Edit] 함수 완성

--- student.cpp
+++ student.cpp
@@ -4,31 +4,36 @@
 
 /* StudentRecord */
 
-StudentRecord::StudentRecord(float midterm, float final) 
+StudentRecord::StudentRecord(float midterm, float final) : midterm(midterm), final(final)
 /* TODO: Initializer list 사용해 초기화 해보기 */
 {}
 
 float StudentRecord::getTotal() const {
     /* TODO */
-    return 0.0f;
+    return getMidterm() + getFinal();
 }
 
 void StudentRecord::updateScores(float midterm, float final) {
     /* TODO: this keyword 사용해 보기 */
+    // c++에서 this는 포인터입니다. 때문에 (*this).var 도 가능합니다만, 번거롭기 떼문에 this->var 를 사용합니다.
+    this->midterm = midterm;
+    (*this).final = final;
 }
 
 
 /* Student */
 
-Student::Student() 
+Student::Student() : name(""), id(-1), record(0,0)
 /* TODO: Initializer list 사용해 초기화 해보기 */
 { }
 
-Student::Student(const char *name, int id, float midterm, float final)
+Student::Student(const char *name, int id, float midterm, float final) : name(name), id(id), record(midterm, final)
 /* TODO: Initializer list 사용해 초기화 해보기 */
 {  }
 
 void Student::updateRecord(const char *name, float midterm, float final) {
     /* TODO */
+    this->name = name;
+    (this->record).updateScores(midterm, final);
 }
 
--- student_manager.cpp
+++ student_manager.cpp
@@ -7,49 +7,86 @@ int StudentManager::last_student_id = 1000;
 void StudentManager::addStudent(char const* name, float midterm, float final) {
     int id = ++last_student_id;
     /* TODO */
-
+    // Student st = {name, id, midterm, final};
+    students[num_of_students++] = {name, id, midterm, final};
 }
 
 void StudentManager::deleteStudent(int id) {
     /* TODO */
+    if (findStudentByID(id) > 0){
+        for (int i=id + 1; i<num_of_students; i++) {
+            students[i-1] = students[i];
+        }
+        num_of_students--;
+    }
 }
 
 void StudentManager::modifyStudent(const Student& student) {
     /* TODO */
+    if (findStudentByID(student.getID()) > -1)
+        students[student.getID() - 1000] = student;
 }
 
 int StudentManager::findStudentByID(int id) const {
     /* TODO */
+    for (int idx=0; idx<num_of_students; idx++) {
+        if (students[idx].getID() == id){
+            return idx;
+        }
+    }
     return -1;
 }
 
 int StudentManager::findBestStudentInMidterm() const {
     /* TODO */
-    return -1;
+    if (last_student_id <= 1000)    return -1;
+    Student best = students[0];
+    for (int idx=0; idx<num_of_students; idx++) {
+        best = best.getRecord().getMidterm() < students[idx].getRecord().getMidterm() ? students[idx] : best;
+    }
+    return best.getRecord().getMidterm();
 }
 
 int StudentManager::findBestStudentInFinal() const {
     /* TODO */
-    return -1;
+    if (last_student_id <= 1000)    return -1;
+    Student best = students[0];
+    for (int idx=0; idx<num_of_students; idx++) {
+        best = best.getRecord().getFinal() < students[idx].getRecord().getFinal() ? students[idx] : best;
+    }
+    return best.getRecord().getFinal();
 }
 
 int StudentManager::findBestStudent() const {
     /* TODO */
-    return -1;
+    if (last_student_id <= 1000)    return -1;
+    Student best = students[0];
+    for (int idx=0; idx<num_of_students; idx++) {
+        best = best.getRecord().getFinal() < students[idx].getRecord().getFinal() ? students[idx] : best;
+    }
+    return best.getRecord().getTotal();
 }
 
 float StudentManager::getMidtermAverage() const {
     /* TODO */
-    return 0.0f;
+    float sum = 0.0f;
+
+    for (int i=0; i<num_of_students; i++) 
+        sum += students[i].getRecord().getMidterm();
+    return sum/num_of_students;
 }
 
 float StudentManager::getFinalAverage() const {
     /* TODO */
-    return 0.0f;
+    float sum = 0.0f;
+    
+    for (int i=0; i<num_of_students; i++) 
+        sum += students[i].getRecord().getFinal();
+    return sum/num_of_students;
 }
 
 float StudentManager::getTotalAverage() const {
     /* TODO */
-    return 0.0f;
+    return ((getMidtermAverage() + getFinalAverage())/2.0f);
 }
 
